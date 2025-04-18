7f5878af7710d58cec181ad45f723f5ac672ec94
KarthickWebDeveloper <karthicklearnings@gmail.com>
2025-04-18 09:56:14 +0530

    Added thinkspeak api

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -34,7 +34,7 @@ const Header = () => {
           <Link to="/">Smart Granary</Link>
         </div>
         <div className="space-x-4">
-          {!isAuthenticated ? (
+          {isAuthenticated ? (
             <>
               <Link to="/login">
                 <button className="px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded-md transition">
